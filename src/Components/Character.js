6114e9a16455dc6e862a7a5c53086870d5f9f1be
abdulhaseebcv <abdulcvhaseeb@gmail.com>
2023-10-07 14:34:48 +0530

    designed search input

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -3,6 +3,9 @@ import './Character.css'
 function Character() {
     return (
       <div>
+        <div className="filter">
+            <input type="text" placeholder='Search' />
+        </div>
         <div className="characters">
             <div className="card">
                 <img className="image" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="character-image" />
@@ -17,7 +20,26 @@ function Character() {
                         <span>Earth (Replacement Dimension)</span>
                     </div>
                     <div className="first-seen">
-                        <p>First seen:</p>
+                        <p>First seen in:</p>
+                        <span>Ricky protion #1</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <img className="image" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="character-image" />
+                <div className="details">
+                    <h2>Abadango Cluster Princess</h2>
+                    <div className="status">
+                        <span></span>
+                        <p>Dead- Human</p>
+                    </div>
+                    <div className="location">
+                        <p>Last known location:</p>
+                        <span>Earth (Replacement Dimension)</span>
+                    </div>
+                    <div className="first-seen">
+                        <p>First seen in:</p>
                         <span>Ricky protion #1</span>
                     </div>
                 </div>
